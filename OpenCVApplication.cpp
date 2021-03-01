9e478398a8a572f0a194103b7dabfb20162ad370
Valeriu Sabau <valeriusabau29@gmail.com>
2021-03-01 13:18:33 +0200

    change the main loop. finished lab2

--- OpenCVApplication.cpp
+++ OpenCVApplication.cpp
@@ -5,6 +5,12 @@
 #include "common.h"
 #include <algorithm>
 #include <iostream>
+#include <vector>
+#include <utility>
+#include <functional>
+
+std::vector<std::pair<std::string, std::function <void() > > > functionSet;
+
 void testOpenImage()
 {
 	char fname[MAX_PATH];
@@ -432,6 +438,8 @@ uchar bound(int val)
 
 }
 
+
+
 void lab1Problem3()
 {
 	char fname[MAX_PATH];
@@ -459,6 +467,8 @@ void lab1Problem3()
 	}
 }
 
+
+
 void lab1Problem4()
 {
 	char fname[MAX_PATH];
@@ -476,7 +486,7 @@ void lab1Problem4()
 			{
 				uchar val = src.at<uchar>(i, j);
 				val = bound((int)val * multiplicativeFactor);
-				dst.at<uchar>(i, j) = multiplicativeFactor * val;
+				dst.at<uchar>(i, j) = val;
 			}
 		}
 
@@ -558,7 +568,243 @@ void lab1Problem6()
 
 }
 
-int main()
+void lab2Problem1()
+{
+
+	char fname[MAX_PATH];
+	while (openFileDlg(fname)) {
+
+		Mat src = imread(fname, IMREAD_COLOR);
+		int height = src.rows;
+		int width = src.cols;
+
+		Mat dstB = Mat(height, width, CV_8UC1);
+		Mat dstR = Mat(height, width, CV_8UC1);
+		Mat dstG = Mat(height, width, CV_8UC1);
+
+
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				Vec3b val = src.at<Vec3b>(i, j);
+				dstB.at<uchar>(i, j) = val[0];
+				dstG.at<uchar>(i, j) = val[1];
+				dstR.at<uchar>(i, j) = val[2];
+	
+
+			}
+		}
+
+		imshow("input image", src);
+		imshow("B", dstB);
+		imshow("R", dstR);
+		imshow("G", dstG);
+		waitKey();
+	}
+}
+
+
+void lab2Problem2()
+{
+	char fname[MAX_PATH];
+	while (openFileDlg(fname)) {
+
+		Mat src = imread(fname, IMREAD_COLOR);
+		int height = src.rows;
+		int width = src.cols;
+
+		Mat dst = Mat(height, width, CV_8UC1);
+	
+
+
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				Vec3b val = src.at<Vec3b>(i, j);
+				dst.at<uchar>(i, j) = (val[0]+val[1]+val[2])/3;
+	
+
+			}
+		}
+		imshow("input image", src);
+		imshow("grayscale", dst);
+		waitKey();
+	}
+}
+
+void lab2Problem3()
+{
+	char fname[MAX_PATH];
+
+	while (openFileDlg(fname)) {
+
+		Mat src = imread(fname, IMREAD_GRAYSCALE);
+		int height = src.rows;
+		int width = src.cols;
+
+		Mat dst = Mat(height, width, CV_8UC1);
+		uchar treshold;
+		std::cout << "treshold:";
+		std::cin >> treshold;
+
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				uchar val = src.at<uchar>(i, j);
+				dst.at<uchar>(i, j) = (val < treshold) ? 0 : 255;
+			}
+		}
+		imshow("input image", src);
+		imshow("grayscale", dst);
+		waitKey();
+	}
+}
+
+void lab2Problem4()
+{
+	char fname[MAX_PATH];
+	while (openFileDlg(fname)) {
+
+		Mat src = imread(fname, IMREAD_COLOR);
+		int height = src.rows;
+		int width = src.cols;
+
+		Mat dstH = Mat(height, width, CV_8UC1);
+		Mat dstS = Mat(height, width, CV_8UC1);
+		Mat dstV = Mat(height, width, CV_8UC1);
+
+
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				Vec3b val = src.at<Vec3b>(i, j);
+				float b = (float)val[0] / 255;
+				float g = (float)val[1] / 255;
+				float r = (float)val[2] / 255;
+				float M = fmax(fmax(b, g), r);
+				float m = fmin(fmin(b, g), r);
+
+				float C = M - m;
+
+				float V = M;
+				float S= (V!=0)? C/V :0;
+
+				float H;
+				if (C != 0)
+				{
+					if (M == r)
+					{
+						H = 60 * (g - b) / C;
+					}
+					if (M == g)
+					{
+						H = 120 + 60 * (b - r) / C;
+					}
+					if (M == b)
+					{
+						H = 240 + 60 * (r - g) / C;
+					}
+				}
+				else
+				{
+					H = 0;
+				}
+
+				if (H < 0)
+					H += 360;
+				// normalize
+				uchar H_norm = H * 255 / 360;
+				uchar S_norm = S * 255;
+				uchar V_norm = V * 255;
+				dstH.at<uchar>(i, j) = H_norm;
+				dstS.at<uchar>(i, j) = S_norm;
+				dstV.at<uchar>(i, j) = V_norm;
+			}
+		}
+
+		imshow("input image", src);
+		imshow("H", dstH);
+		imshow("S", dstS);
+		imshow("V", dstV);
+		waitKey();
+	}
+
+}
+
+bool isInside(Mat image,int i, int j)
+{
+
+	return (0 <= i && i < image.rows && 0 <= j && j < image.cols);
+
+}
+
+void testIsInside()
+{
+	char fname[MAX_PATH];
+
+	while (openFileDlg(fname)) {
+
+		Mat src = imread(fname, IMREAD_COLOR);
+		
+		int i = 0,j=0;
+		std::cout << "test isInside with " << i << " " << j << " result " << isInside(src, i, j) << "\n";
+
+		i = 3000, j = 12000;
+		std::cout << "test isInside with " << i << " " << j << " result " << isInside(src, i, j) << "\n";
+
+
+		i = -10, j = 0;
+		std::cout << "test isInside with " << i << " " << j << " result " << isInside(src, i, j) << "\n";
+		imshow("input image", src);
+	
+		waitKey();
+	}
+}
+
+
+int newMain()
+{
+	int  op;
+	functionSet.push_back(std::make_pair("lab1Problem3", lab1Problem3));
+	functionSet.push_back(std::make_pair("lab1Problem4", lab1Problem4));
+	functionSet.push_back(std::make_pair("lab1Problem5", lab1Problem5));
+	functionSet.push_back(std::make_pair("lab1Problem6", lab1Problem6));
+	functionSet.push_back(std::make_pair("lab2Problem1", lab2Problem1));
+	functionSet.push_back(std::make_pair("lab2Problem2", lab2Problem2));
+	functionSet.push_back(std::make_pair("lab2Problem3", lab2Problem3));
+	functionSet.push_back(std::make_pair("lab2Problem4", lab2Problem4));
+	functionSet.push_back(std::make_pair("testIsInside", testIsInside));
+
+	do {
+		system("cls");
+		destroyAllWindows();
+		printf("Menu:\n");
+		int operationValue = 1;
+		for (auto& it : functionSet)
+		{
+			std::cout << operationValue << "-" << it.first << "\n";
+			operationValue++;
+		}
+		printf("Option: ");
+		scanf("%d", &op);
+		if (op <= functionSet.size() && op>=1)
+		{
+			functionSet[op-1].second();
+		}
+
+	} while (op != 0);
+
+	return 0;
+
+}
+
+
+
+int oldMain()
 {
 	int op;
 	do
@@ -583,6 +829,8 @@ int main()
 		printf(" 0 - Exit\n\n");
 		printf("Option: ");
 		scanf("%d",&op);
+
+
 		switch (op)
 		{
 			case 1:
@@ -625,9 +873,30 @@ int main()
 			case 13:
 				lab1Problem6();
 				break;
+			case 14:
+				lab2Problem1();
+				break;
+			case 15:
+				lab2Problem2();
+				break;
+			case 16:
+				lab2Problem3();
+				break;
+			case 17:
+				lab2Problem4();
+				break;
+			case 18:
+				testIsInside();
+				break;
+
 			
 		}
 	}
 	while (op!=0);
 	return 0;
+}
+
+int main()
+{
+	return newMain();
 }
